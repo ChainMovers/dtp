99c522a484ead7d4fb4c6aaca7e54576cb8567f4
mario4tier <mario4tier@users.noreply.github.com>
2022-12-24 23:23:19 -0500

    (#12) dtp-server websocket events now working with 0.19 localnet

--- crates/dtp-server/src/main.rs
+++ crates/dtp-server/src/main.rs
@@ -5,7 +5,11 @@
 use clap::*;
 use colored::Colorize;
 use std::path::PathBuf;
-use telemetry_subscribers;
+use telemetry_subscribers::TelemetryConfig;
+
+use futures::StreamExt;
+use sui_sdk::rpc_types::SuiEventFilter;
+use sui_sdk::SuiClient;
 
 #[allow(clippy::large_enum_variant)]
 #[derive(Parser)]
@@ -28,8 +32,18 @@ impl Command {
     pub async fn execute(self) -> Result<(), anyhow::Error> {
         match self {
             Command::Localnet { path } => {
-                if let Some(x) = path {
-                    println!("{}", x.into_os_string().into_string().unwrap());
+                if let Some(_x) = path {
+                    let sui =
+                        SuiClient::new("http://0.0.0.0:9000", Some("ws://0.0.0.0:9000"), None)
+                            .await?;
+                    let mut subscribe_all = sui
+                        .event_api()
+                        .subscribe_event(SuiEventFilter::All(vec![]))
+                        .await?;
+                    loop {
+                        println!("{:?}", subscribe_all.next().await);
+                    }
+                    //println!("{}", x.into_os_string().into_string().unwrap());
                 } else {
                     println!("Path not provided");
                 }
@@ -46,9 +60,7 @@ async fn main() {
 
     // TODO Socket for external dtp CLI binary (this is the server not the CLI!)
     // TODO Look into https://crates.io/crates/sentry-tracing for bail/panic logging.
-    let _guard = telemetry_subscribers::TelemetryConfig::new("dtp-server")
-        .with_env()
-        .init();
+    let _guard = TelemetryConfig::new("dtp-server").with_env().init();
 
     let cmd: Command = Command::parse();
 
